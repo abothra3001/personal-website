d64a1732258042b08574c8c9589f64fd4d061fae
abothra3001 <abothra3001@gmail.com>
2020-06-22 14:33:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route, Switch,} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch,} from 'react-router-dom';
 import { Home } from './Home';
 import { Music } from './Music';
 import { FullCV } from './FullCV';
