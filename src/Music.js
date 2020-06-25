e08871e325393f30402668de88ac26a1c2824bc9
Anjali Bothra <anjalibothra@Anjalis-MacBook-Pro-2.local>
2020-06-25 01:04:52 -0400

    changed favicon and fixed youtube links

--- src/Music.js
+++ src/Music.js
@@ -1,22 +1,26 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 import solitude from './assets/Solitude.mp4'
+
 export const Music = () => (
   <div>
     <h1> In my free time I like to make music!</h1>
-    <div className = "padding50"></div>
+    <div className = "padding20"></div>
     <p> This quarantine I've been mixing and producing parodies and originals. Here's my first parody, Brown and Boujee! Be sure to check out my youtube channel to see the rest.</p>
 
-    <div className="embed-responsive embed-responsive-16by9">
-    <iframe title="embedsPage" className="embed-responsive-item" src= "https://www.youtube.com/watch?v=dEaNcLlFuB0"
-    allowfullscreen></iframe>
+    <div className = "padding20"></div>
+    <div align = "center">
+    <ReactPlayer url = "https://www.youtube.com/watch?v=dEaNcLlFuB0" width = {1000} height = {600}/>
     </div>
 
-    <div className = "padding50"></div>
+    <div className = "padding20"></div>
     <p> If you'd like to listen to something more serious. Here is a piece I composed in my junior year of high school called Solitude. </p>
-    <div className="embed-responsive embed-responsive-16by9">
-    <iframe title="embedsPage" className="embed-responsive-item" src= {solitude} allowfullscreen></iframe>
+
+    <div align = "center">
+    <ReactPlayer url = {solitude} controls = {true} width = {1000} height = {600}/>
     </div>
 
+    <div className = "padding20"></div>
     <p> I post a lot of covers on my instagram: @jellibelli_sings. </p>
   </div>
 )
